5197b97071a6fee0d1ce2b1b3f7c68559f476c39
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-21 02:33:36 +0100

    Init

---
+++ snack 1/index.php
@@ -0,0 +1,32 @@
+<!-- Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+
+Olimpia Milano - Cantù | 55-60 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php
+
+        $match = [
+            'home' => 'Olimpia Milano',
+            'away' => 'Cantù',
+            'homePoints' => 55,
+            'awayPoints' => 60
+        ]
+    ?>
+</head>
+<body>
+    <p>
+        <?php
+            for ($i=0; $i<; $i++) {
+
+            }
+        ?>    
+    </p>
+</body>
+</html>
